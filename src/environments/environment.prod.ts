4884fc8899d168343063e03bf3289ea8cd47b82c
shukla07 <shivama98@gmail.com>
2021-05-17 23:06:38 +0530

    Updated prod env file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'api.openweathermap.org/data/2.5',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
   apiKey: 'a4bc912d577ea0061864e93a9d71d09d',
 };
